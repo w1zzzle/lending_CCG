8514eb266465cef80e399c7b34d0d42e7010f737
DimaGenkai <92301623+w1zzzle@users.noreply.github.com>
2023-06-20 16:11:12 +0400

    scss fixing

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function init() {
         balloonContentHeader: '<a href = "#">Cyber Club Glazov #1</a><br>' +
             '<span class="description">Компьютерный клуб</span>',
         // Зададим содержимое основной части балуна.
-        balloonContentBody: '<img href="https://vk.link/fungamersglazov" src="img/logo.jpg" height="100" width="100"> <br/> ' +
+        balloonContentBody: '<img href="https://vk.link/fungamersglazov" src="img/logo.png" height="100" width="100"> <br/> ' +
             '<a href="tel:+79097135329">+7 (909) 713-53-29</a><br/>',
         // Зададим содержимое нижней части балуна.
         balloonContentFooter: 'вс-чт 11:00 - 23:00<br/>пт, сб 11:00 - 04:00',
@@ -31,7 +31,7 @@ function init() {
     map.controls.remove("searchControl"); // удаляем поиск
     map.controls.remove("trafficControl"); // удаляем контроль трафика
     map.controls.remove("typeSelector"); // удаляем тип
-    map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+    // map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
     // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
     map.controls.remove("rulerControl"); // удаляем контрол правил
     map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
